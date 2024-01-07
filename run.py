285d4f78423aa95656f62be96385a109a3d29c74
ShieldsJohn <>
2024-01-07 15:06:15 +0000

    Start run_game function with check/response to first guessed letter

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def musician_blanks():
     on the game screen
     """
     musician = random.choice(musicians_list).upper()
-    return "_" * len(musician)
+    return musician
 
 
 def letter_validation():
@@ -17,14 +17,31 @@ def letter_validation():
         try:
             letter_input = input("Please enter a letter: \n").upper()
             if len(letter_input) == 1 and letter_input.isalpha():
-                break
+                return letter_input.upper()
             else:
-                print("Please enter a single letter - try again!\n")
+                print("Invalid - please enter a letter.\n")
         except ValueError:
             print("Enter one letter.\n")
     return letter_input
 
+
 tries = 6
+guessed_letters = []
+guessed = False
+display = " "
+musician = musician_blanks()
+list_musician = list(musician)
+letter_input = letter_validation()
+
+
+def run_game(musician):
+    for letter_input in list_musician:
+        if letter_input in list_musician:
+            guessed = True
+            print("Well done - correct guess!")
+            break
+        else:
+            print("This letter is not in the word - please try again!")
 
 
 def display_dj(tries):
@@ -112,6 +129,7 @@ def main():
     print(display_dj(tries))
     print(musician_blanks())
     letter_validation()
+    run_game(musician)
 
 
 main()
